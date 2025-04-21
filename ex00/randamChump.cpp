0b4cbaf1a1fc1e70b060007af724ddf210ad4c14
root <sushi.honda@gmail.com>
2025-04-21 10:47:26 +0800

    ex00) add: randamChump (stack memory)
      because of subject requirement, return value is void.
      different from newZombie case

---
+++ ex00/randamChump.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randamChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 09:56:13 by athonda           #+#    #+#             */
+/*   Updated: 2025/04/21 10:47:09 by athonda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include "Zombie.hpp"
+
+void	randamChump(std::string name)
+{
+	Zombie	zombie(name);
+
+}
