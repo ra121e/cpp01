1a19645b2bd567fbcb3c904d441b1d907adeffe2
root <sushi.honda@gmail.com>
2025-04-21 18:02:13 +0800

    ex01) preparation
      Zombie.hpp: minimum set
      Zombie.cpp: constructor, destructor

---
+++ ex01/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 17:16:15 by athonda           #+#    #+#             */
+/*   Updated: 2025/04/21 17:22:06 by athonda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+
+class Zombie
+{
+public:
+	Zombie(std::string name);
+	~Zombie();
+
+private:
+	std::string	name;
+	Zombie();
+}
+
+#endif
