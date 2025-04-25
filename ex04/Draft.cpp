0f2044cad3d296b12df3a8537d995139e53e9fbe
root <sushi.honda@gmail.com>
2025-04-25 17:52:53 +0800

    ex04) update: replace function
      step2 is repalce function
      find the word by .find then get position
      delete the word by .erase by the postion
      insert new word by .insert
      the advance position

--- ex04/Draft.cpp
+++ ex04/Draft.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 18:02:49 by athonda           #+#    #+#             */
-/*   Updated: 2025/04/25 14:39:16 by athonda          ###   ########.fr       */
+/*   Updated: 2025/04/25 17:45:14 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 Draft::Draft(const std::string &filename, std::string &s1, std::string &s2) :
 	_filename(filename),
-	_si(s1),
+	_s1(s1),
 	_s2(s2),
 	_content("")
 {}
@@ -40,9 +40,16 @@ bool	Draft::readFile()
 	return (true);
 }
 
-void	replaceWord()
+void	Draft::replaceWord()
 {
-
+	std::string	result = "";
+	std::size_t	pos = 0;
+	while ((pos =_content.find(_s1, pos)) != std::string::npos)
+	{
+		_content.erase(pos, _s1.length());
+		_content.insert(pos, _s2);
+		pos += _s2.length();
+	}
 }
 
 void	writeFile()
@@ -55,8 +62,8 @@ bool	Draft::replace()
 {
 	if (!readFile())
 		return (false);
+	replaceWord();
 	display();
-//	replaceWord();
 //	writeFile();
 	return (true);
 }
--- ex04/Draft.hpp
+++ ex04/Draft.hpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 17:46:20 by athonda           #+#    #+#             */
-/*   Updated: 2025/04/25 14:38:23 by athonda          ###   ########.fr       */
+/*   Updated: 2025/04/25 17:34:13 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@
 class Draft
 {
 public:
-	Draft(const std::string &filename, std::string &si, std::string &s2);
+	Draft(const std::string &filename, std::string &s1, std::string &s2);
 	~Draft();
 	bool	replace();
 
 private:
 	std::string	_filename;
-	std::string	_si;
+	std::string	_s1;
 	std::string	_s2;
 	std::string	_content;
 
